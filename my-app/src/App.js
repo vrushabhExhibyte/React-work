ae9b66d9d81900c6392ceb3af01b3b69c02e1dd5
vrushabhExhibyte <vrushabh01.exhibyte@gmail.com>
2021-07-06 18:28:11 +0530

    commit

---
+++ my-app/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./App.css";
+import ToDoList from "./ToDoList";
+// import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+// import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
+
+function App() {
+  return <ToDoList />;
+}
+export default App;
+
+// const [count, setCount] = useState(0);
+
+{
+  /* <form>
+  <label>
+    Name:
+    <input type="text" placeholder="enter your name" name="name" /><br/><br/>
+  </label>
+  <input type="submit" value="Submit" />
+</form> */
+}
+{
+  /* <select>
+  <option selected value="deafult">choose</option>
+  <option value="grapefruit">Grapefruit</option>
+  <option value="lime">Lime</option>
+  <option value="coconut">Coconut</option>
+  <option value="mango">Mango</option>
+</select> */
+}
+
+//   return (
+//     <div className="App-header">
+//       <h1>Name of the king</h1>
+//       <p>You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>Click me</button>
+//       <button onClick={() => setCount(count - 1)}>Click</button>
+//     </div>
+//   );
+// }
---
+++ my-app/src/ToDoList.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+
+const ToDoList = () => {
+  const [item, setItem] = useState("");
+  const [newItem, setNewItem] = useState([]);
+
+  const itemEvent = (event) => {
+    setItem(event.target.value);
+  };
+  const listItem = () => {
+    setNewItem((preValue) => {
+      return [...preValue, item];
+    });
+    setItem("");
+  };
+  return (
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <h1>ToDoList</h1>
+          <input
+            type="text"
+            value={item}
+            placeholder="enter name"
+            onChange={itemEvent}
+          />
+          <Button className="newbutton" onClick={listItem}>
+            <AddCircleIcon />
+          </Button>
+          <ol>
+            {newItem.map((val) => {
+              return <li>{val}</li>;
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ToDoList;
